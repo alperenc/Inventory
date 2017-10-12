876ba4df8341afb98ff9ca1171e6da553b50d2fa
Alp Eren Can <alperenc@gmail.com>
2017-10-12 18:05:48 +0300

    add constants for product content URIs

--- app/src/main/java/com/alperencan/inventory/android/data/InventoryContract.java
+++ app/src/main/java/com/alperencan/inventory/android/data/InventoryContract.java
@@ -1,5 +1,6 @@
 package com.alperencan.inventory.android.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -7,6 +8,23 @@ import android.provider.BaseColumns;
  */
 
 public final class InventoryContract {
+
+    /**
+     * The "Content authority" is a name for the entire content provider, similar to the
+     * relationship between a domain name and its website.  A convenient string to use for the
+     * content authority is the package name for the app, which is guaranteed to be unique on the
+     * device.
+     */
+    public static final String CONTENT_AUTHORITY = "com.alperencan.inventory.android";
+    /**
+     * Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+     * the content provider.
+     */
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    /**
+     * Possible path (appended to base content URI for possible URI's)
+     */
+    public static final String PATH_PRODUCTS = "products";
 
     // To prevent someone from accidentally instantiating the contract class,
     // give it an empty constructor.
@@ -17,6 +35,11 @@ public final class InventoryContract {
      * Each entry in the table represents a single inventory item.
      */
     public static final class InventoryEntry implements BaseColumns {
+
+        /**
+         * The content URI to access the product data in the provider
+         */
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PRODUCTS);
 
         /** Name of database table for products */
         public final static String TABLE_NAME = "products";
