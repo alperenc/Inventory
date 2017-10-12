2b80b213388a8ec1bf279b474a96d7cb13709b38
Alp Eren Can <alperenc@gmail.com>
2017-10-12 09:08:08 +0300

    add InventoryContract class

---
+++ app/src/main/java/com/alperencan/inventory/android/data/InventoryContract.java
@@ -0,0 +1,62 @@
+package com.alperencan.inventory.android.data;
+
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+import java.util.Set;
+
+/**
+ * API contract for the Inventory app.
+ */
+
+public final class InventoryContract {
+
+    // To prevent someone from accidentally instantiating the contract class,
+    // give it an empty constructor.
+    private InventoryContract() {}
+
+    /**
+     * Inner class that defines constant values for the inventory database table.
+     * Each entry in the table represents a single inventory item.
+     */
+    public static final class InventoryEntry implements BaseColumns {
+
+        /** Name of database table for products */
+        public final static String TABLE_NAME = "products";
+
+        /**
+         * Unique ID number for the product (only for use in the database table).
+         *
+         * Type: INTEGER
+         */
+        public final static String _ID = BaseColumns._ID;
+
+        /**
+         * Name of the product.
+         *
+         * Type: TEXT
+         */
+        public final static String COLUMN_PRODUCT_NAME ="name";
+
+        /**
+         * Quantity of the product.
+         *
+         * TYPE: INTEGER
+         */
+        public final static String COLUMN_PRODUCT_QUANTITY= "quantity";
+
+        /**
+         * Price of the product.
+         *
+         * TYPE: INTEGER
+         */
+        public final static String COLUMN_PRODUCT_PRICE = "price";
+
+        /**
+         * Photo of the product.
+         *
+         * TYPE: BLOB
+         */
+        public final static String COLUMN_PRODUCT_PHOTO = "photo";
+    }
+}
