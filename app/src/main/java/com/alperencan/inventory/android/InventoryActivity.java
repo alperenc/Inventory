d7e9103fad6544d24624e35afb63417b8592f976
Alp Eren Can <alperenc@gmail.com>
2017-10-15 23:16:19 +0300

    create ProductCursorAdapter to display list of products in ListView

--- app/src/main/java/com/alperencan/inventory/android/InventoryActivity.java
+++ app/src/main/java/com/alperencan/inventory/android/InventoryActivity.java
@@ -1,6 +1,7 @@
 package com.alperencan.inventory.android;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,7 +10,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ListView;
 
+import com.alperencan.inventory.android.adapter.ProductCursorAdapter;
 import com.alperencan.inventory.android.data.InventoryContract.InventoryEntry;
 
 public class InventoryActivity extends AppCompatActivity {
@@ -32,6 +35,40 @@ public class InventoryActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        displayDatabaseInfo();
+    }
+
+    /**
+     * Temporary helper method to display information in the onscreen ListView about the state of
+     * the products database.
+     */
+    private void displayDatabaseInfo() {
+        // Perform a query on the provider using the ContentResolver.
+        // Use the {@link InventoryEntry#CONTENT_URI} to access the product data.
+        Cursor cursor = getContentResolver().query(
+                InventoryEntry.CONTENT_URI,   // The content URI of the products table
+                null,                   // The columns to return for each row (null for all columns)
+                null,                   // Selection criteria
+                null,                   // Selection criteria
+                null);                  // The sort order for the returned rows
+
+        // Find the ListView which will be populated with the product data
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        // Find and set empty view on the ListView, so that it only shows when the list has 0 items.
+        View emptyView = findViewById(R.id.empty_view);
+        listView.setEmptyView(emptyView);
+
+        // Setup an Adapter to create a list item for each row of product data in the Cursor.
+        ProductCursorAdapter adapter = new ProductCursorAdapter(this, cursor);
+
+        // Attach the adapter to the ListView.
+        listView.setAdapter(adapter);
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_options, menu);
@@ -48,6 +85,7 @@ public class InventoryActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_add_products) {
             addDummyProducts();
+            displayDatabaseInfo();
             return true;
         }
 
@@ -61,7 +99,7 @@ public class InventoryActivity extends AppCompatActivity {
         iPhone8Values.put(InventoryEntry.COLUMN_PRODUCT_NAME, "iPhone 8");
         iPhone8Values.put(InventoryEntry.COLUMN_PRODUCT_QUANTITY, 100);
         iPhone8Values.put(InventoryEntry.COLUMN_PRODUCT_PRICE, 699.00);
-        iPhone8Values.put(InventoryEntry.COLUMN_PRODUCT_PHOTO_URL, "android.resource://com.alperencan.inventory.android/res/drawable/iphone8.png");
+        iPhone8Values.put(InventoryEntry.COLUMN_PRODUCT_PHOTO_URL, "android.resource://com.alperencan.inventory.android/drawable/iphone8");
 
         // Insert a new row for product in the database
         getContentResolver().insert(InventoryEntry.CONTENT_URI, iPhone8Values);
@@ -72,7 +110,7 @@ public class InventoryActivity extends AppCompatActivity {
         iPhone8PlusValues.put(InventoryEntry.COLUMN_PRODUCT_NAME, "iPhone 8+");
         iPhone8PlusValues.put(InventoryEntry.COLUMN_PRODUCT_QUANTITY, 100);
         iPhone8PlusValues.put(InventoryEntry.COLUMN_PRODUCT_PRICE, 799.00);
-        iPhone8PlusValues.put(InventoryEntry.COLUMN_PRODUCT_PHOTO_URL, "android.resource://com.alperencan.inventory.android/res/drawable/iphone8_plus.png");
+        iPhone8PlusValues.put(InventoryEntry.COLUMN_PRODUCT_PHOTO_URL, "android.resource://com.alperencan.inventory.android/drawable/iphone8_plus");
 
         // Insert a new row for product in the database
         getContentResolver().insert(InventoryEntry.CONTENT_URI, iPhone8PlusValues);
@@ -83,7 +121,7 @@ public class InventoryActivity extends AppCompatActivity {
         iPhoneXValues.put(InventoryEntry.COLUMN_PRODUCT_NAME, "iPhone X");
         iPhoneXValues.put(InventoryEntry.COLUMN_PRODUCT_QUANTITY, 100);
         iPhoneXValues.put(InventoryEntry.COLUMN_PRODUCT_PRICE, 999.00);
-        iPhoneXValues.put(InventoryEntry.COLUMN_PRODUCT_PHOTO_URL, "android.resource://com.alperencan.inventory.android/res/drawable/iphone_x.png");
+        iPhoneXValues.put(InventoryEntry.COLUMN_PRODUCT_PHOTO_URL, "android.resource://com.alperencan.inventory.android/drawable/iphone_x");
 
         // Insert a new row for product in the database
         getContentResolver().insert(InventoryEntry.CONTENT_URI, iPhoneXValues);
@@ -94,7 +132,7 @@ public class InventoryActivity extends AppCompatActivity {
         Pixel2Values.put(InventoryEntry.COLUMN_PRODUCT_NAME, "Pixel 2");
         Pixel2Values.put(InventoryEntry.COLUMN_PRODUCT_QUANTITY, 100);
         Pixel2Values.put(InventoryEntry.COLUMN_PRODUCT_PRICE, 649.00);
-        Pixel2Values.put(InventoryEntry.COLUMN_PRODUCT_PHOTO_URL, "android.resource://com.alperencan.inventory.android/res/drawable/pixel2.png");
+        Pixel2Values.put(InventoryEntry.COLUMN_PRODUCT_PHOTO_URL, "android.resource://com.alperencan.inventory.android/drawable/pixel2");
 
         // Insert a new row for product in the database
         getContentResolver().insert(InventoryEntry.CONTENT_URI, Pixel2Values);
@@ -105,7 +143,7 @@ public class InventoryActivity extends AppCompatActivity {
         Pixel2XLValues.put(InventoryEntry.COLUMN_PRODUCT_NAME, "Pixel 2 XL");
         Pixel2XLValues.put(InventoryEntry.COLUMN_PRODUCT_QUANTITY, 100);
         Pixel2XLValues.put(InventoryEntry.COLUMN_PRODUCT_PRICE, 849.00);
-        Pixel2XLValues.put(InventoryEntry.COLUMN_PRODUCT_PHOTO_URL, "android.resource://com.alperencan.inventory.android/res/drawable/pixel2_xl.png");
+        Pixel2XLValues.put(InventoryEntry.COLUMN_PRODUCT_PHOTO_URL, "android.resource://com.alperencan.inventory.android/drawable/pixel2");
 
         // Insert a new row for product in the database
         getContentResolver().insert(InventoryEntry.CONTENT_URI, Pixel2XLValues);
---
+++ app/src/main/java/com/alperencan/inventory/android/adapter/ProductCursorAdapter.java
@@ -0,0 +1,78 @@
+package com.alperencan.inventory.android.adapter;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.alperencan.inventory.android.R;
+import com.alperencan.inventory.android.data.InventoryContract.InventoryEntry;
+
+/**
+ * {@link ProductCursorAdapter} is an adapter for a list or grid view
+ * that uses a {@link Cursor} of product data as its data source. This adapter knows
+ * how to create list items for each row of pet data in the {@link Cursor}.
+ */
+
+public class ProductCursorAdapter extends CursorAdapter {
+
+    /**
+     * Constructs a new {@link ProductCursorAdapter}.
+     *
+     * @param context The context
+     * @param c       The cursor from which to get the data.
+     */
+    public ProductCursorAdapter(Context context, Cursor c) {
+        super(context, c, 0 /* flags */);
+    }
+
+    /**
+     * Makes a new blank list item view. No data is set (or bound) to the views yet.
+     *
+     * @param context app context
+     * @param cursor  The cursor from which to get the data. The cursor is already
+     *                moved to the correct position.
+     * @param parent  The parent to which the new view is attached to
+     * @return the newly created list item view.
+     */
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
+    }
+
+    /**
+     * This method binds the product data (in the current row pointed to by cursor) to the given
+     * list item layout. For example, the name for the current product can be set on the name TextView
+     * in the list item layout.
+     *
+     * @param view    Existing view, returned earlier by newView() method
+     * @param context app context
+     * @param cursor  The cursor from which to get the data. The cursor is already moved to the
+     *                correct row.
+     */
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        // Find individual views that we want to modify in the list item layout
+        ImageView productImageView = view.findViewById(R.id.product_image);
+        TextView nameTextView = view.findViewById(R.id.name_text);
+        TextView quantityTextView = view.findViewById(R.id.quantity_text);
+        TextView priceTextView = view.findViewById(R.id.price_text);
+
+        // Find the columns of the product attributes from the Cursor for the current product
+        Uri imageUri = Uri.parse(cursor.getString(cursor.getColumnIndex(InventoryEntry.COLUMN_PRODUCT_PHOTO_URL)));
+        String name = cursor.getString(cursor.getColumnIndex(InventoryEntry.COLUMN_PRODUCT_NAME));
+        int quantity = cursor.getInt(cursor.getColumnIndex(InventoryEntry.COLUMN_PRODUCT_QUANTITY));
+        float price = cursor.getFloat(cursor.getColumnIndex(InventoryEntry.COLUMN_PRODUCT_PRICE));
+
+        // Update the views with the attributes for the current product
+        productImageView.setImageURI(imageUri);
+        nameTextView.setText(name);
+        quantityTextView.setText(String.valueOf(quantity));
+        priceTextView.setText(String.valueOf(price));
+    }
+}
