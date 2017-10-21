3d14ffac6ace73e2e130b6c107658b7f64454296
Alp Eren Can <alperenc@gmail.com>
2017-10-21 16:08:14 +0300

    display “Price: TBD” for product without price

--- app/src/main/java/com/alperencan/inventory/android/adapter/ProductCursorAdapter.java
+++ app/src/main/java/com/alperencan/inventory/android/adapter/ProductCursorAdapter.java
@@ -57,7 +57,7 @@ public class ProductCursorAdapter extends CursorAdapter {
      *                correct row.
      */
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
+    public void bindView(View view, final Context context, Cursor cursor) {
         // Find individual views that we want to modify in the list item layout
         ImageView productImageView = view.findViewById(R.id.product_image);
         TextView nameTextView = view.findViewById(R.id.name_text);
@@ -66,15 +66,22 @@ public class ProductCursorAdapter extends CursorAdapter {
         ImageButton saleImageButton = view.findViewById(R.id.sale_button);
 
         // Find the columns of the product attributes from the Cursor for the current product
-        Uri imageUri = Uri.parse(cursor.getString(cursor.getColumnIndex(InventoryEntry.COLUMN_PRODUCT_PHOTO_URL)));
         String name = cursor.getString(cursor.getColumnIndex(InventoryEntry.COLUMN_PRODUCT_NAME));
+        Uri imageUri = Uri.parse(cursor.getString(cursor.getColumnIndex(InventoryEntry.COLUMN_PRODUCT_PHOTO_URL)));
         int quantity = cursor.getInt(cursor.getColumnIndex(InventoryEntry.COLUMN_PRODUCT_QUANTITY));
         float price = cursor.getFloat(cursor.getColumnIndex(InventoryEntry.COLUMN_PRODUCT_PRICE));
 
         // Update the views with the attributes for the current product
         productImageView.setImageURI(imageUri);
         nameTextView.setText(name);
-        quantityTextView.setText(String.valueOf(quantity));
-        priceTextView.setText(String.valueOf(price));
+        quantityTextView.setText(String.format("%s %s ", context.getString(R.string.quantity), String.valueOf(quantity)));
+
+        // If the product price is 0.0, then use some default text
+        // that says "Price: TBD", so the TextView isn't showing '0.0'.
+        if (price == 0) {
+            priceTextView.setText(String.format("%s %s", context.getString(R.string.price), context.getString(R.string.tbd)));
+        } else {
+            priceTextView.setText(String.format("%s %s", context.getString(R.string.currency), String.valueOf(price)));
+        }
     }
 }
